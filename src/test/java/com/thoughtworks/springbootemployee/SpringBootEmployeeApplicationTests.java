0df9effd36a0dbf383cbbcf71bcd2e3d9529af0b
Mikearce Pascual <PASCUMI3@OOCL.COM>
2023-08-23 13:57:55 +0800

    feat: added test for should_return_the_employees_by_given_gender_when_perform_get_employees

--- src/test/java/com/thoughtworks/springbootemployee/SpringBootEmployeeApplicationTests.java
+++ src/test/java/com/thoughtworks/springbootemployee/SpringBootEmployeeApplicationTests.java
@@ -71,4 +71,21 @@ class SpringBootEmployeeApplicationTests {
 				.andExpect(status().isNotFound());
 	 //then
 	}
+	@Test
+	void should_return_the_employees_by_given_gender_when_perform_get_employees() throws Exception{
+		//given
+		Employee alice = employeeRepository.addEmployee(
+				new Employee("Alice",24,"Female",9000,1L));
+		employeeRepository.addEmployee(new Employee("Bob",28,"Male",8000,2L));
+		//when, then
+		mockMvcClient.perform(MockMvcRequestBuilders.get("/employees").param("gender","Female"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$", hasSize(1)))
+				.andExpect(jsonPath("$[0].id").value(alice.getId()))
+				.andExpect(jsonPath("$[0].name").value(alice.getName()))
+				.andExpect(jsonPath("$[0].age").value(alice.getAge()))
+				.andExpect(jsonPath("$[0].gender").value(alice.getGender()))
+				.andExpect(jsonPath("$[0].salary").value(alice.getSalary()))
+				.andExpect(jsonPath("$[0].companyId").value(alice.getCompanyId()));
+	}
 }
